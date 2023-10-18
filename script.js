b6b3f73e89937cc9d1818402a0df25d42feaca28
EasyJake <EasyJake@users.noreply.github.com>
2023-10-17 18:35:10 -0700

    added bow draw mechanic
    The previous commit allowed the projectile to laugh without the bow being drawn, this code corrects that. Promising pieces of code saved as unused .js files beginning with ~

--- script.js
+++ script.js
@@ -10,113 +10,71 @@ document.addEventListener('DOMContentLoaded', function() {
     circle.style.bottom = '70px';
     circle.style.left = '50%';
     circle.style.transform = 'translateX(-50%)';
-    circle.style.cursor = 'pointer'; // Cursor indicates it's draggable
+    circle.style.cursor = 'pointer'; 
 
     container.appendChild(circle);
 
     let isDragging = false;
     let startDragY = 0;
     let startCircleBottom = 0;
-    const radius = 25; // Half of the circle's diameter
+    const radius = 25;
+
+    let initialPosition = null;
+    let tension = 0;
 
     circle.addEventListener('mousedown', function(event) {
         isDragging = true;
         startDragY = event.clientY;
         startCircleBottom = parseFloat(circle.style.bottom);
-        event.preventDefault(); // Prevent text selection, etc.
+        initialPosition = { x: event.clientX, y: event.clientY };
+        event.preventDefault();
     });
 
     document.addEventListener('mousemove', function(event) {
         if (!isDragging) return;
 
+        const currentPosition = { x: event.clientX, y: event.clientY };
+        tension = Math.sqrt(Math.pow(currentPosition.x - initialPosition.x, 2) + Math.pow(currentPosition.y - initialPosition.y, 2));
+        
         let distanceMoved = startDragY - event.clientY;
-
-        // Limit the distance to the radius of the circle
-        if (distanceMoved > radius) {
-            distanceMoved = radius;
-        } else if (distanceMoved < -radius) {
-            distanceMoved = -radius;
-        }
-
+        if (distanceMoved > radius) distanceMoved = radius;
+        else if (distanceMoved < -radius) distanceMoved = -radius;
+        
         circle.style.bottom = (startCircleBottom + distanceMoved) + 'px';
     });
 
     document.addEventListener('mouseup', function() {
         if (!isDragging) return;
-        isDragging = false;
 
-        // Create and animate the projectile
-        const projectile = document.createElement('div');
-        projectile.style.width = '25px';
-        projectile.style.height = '25px';
-        projectile.style.backgroundColor = 'blue';
-        projectile.style.borderRadius = '50%';
-        projectile.style.position = 'absolute';
-        projectile.style.bottom = (parseFloat(circle.style.bottom) + 25) + 'px';
-        projectile.style.left = '50%';
-        projectile.style.transform = 'translateX(-50%)';
-        container.appendChild(projectile);
+        if (tension > radius) { // only shoot if there's proper tension
+            const projectile = document.createElement('div');
+            projectile.style.width = '25px';
+            projectile.style.height = '25px';
+            projectile.style.backgroundColor = 'blue';
+            projectile.style.borderRadius = '50%';
+            projectile.style.position = 'absolute';
+            projectile.style.bottom = (parseFloat(circle.style.bottom) + 25) + 'px';
+            projectile.style.left = '50%';
+            projectile.style.transform = 'translateX(-50%)';
+            container.appendChild(projectile);
 
-        requestAnimationFrame(function animate() {
-            const currentBottom = parseFloat(projectile.style.bottom);
-            if (currentBottom < window.innerHeight) {
-                projectile.style.bottom = (currentBottom + 5) + 'px';
-                requestAnimationFrame(animate);
-            } else {
-                container.removeChild(projectile);
-            }
-        });
+            requestAnimationFrame(function animate() {
+                const currentBottom = parseFloat(projectile.style.bottom);
+                if (currentBottom < window.innerHeight) {
+                    projectile.style.bottom = (currentBottom + 5) + 'px';
+                    requestAnimationFrame(animate);
+                } else {
+                    container.removeChild(projectile);
+                }
+            });
+        }
 
         // Animate main circle back to original position
         circle.style.transition = 'bottom 0.5s cubic-bezier(.25,.82,.25,1)';
         circle.style.bottom = '70px';
-
-        // Remove transition once animation is complete
         setTimeout(() => circle.style.transition = '', 500);
+
+        isDragging = false;
+        tension = 0;
     });
-});
-// ... (previous code remains unchanged)
-
-document.addEventListener('mouseup', function() {
-    if (!isDragging) return;
-    isDragging = false;
-
-    // Create and animate the projectile
-    const projectile = document.createElement('div');
-    const sLetter = document.getElementById('sLetter'); // Target the "s" letter
-
-    projectile.style.width = '25px';
-    projectile.style.height = '25px';
-    projectile.style.backgroundColor = 'blue';
-    projectile.style.borderRadius = '50%';
-    projectile.style.position = 'absolute';
-    projectile.style.bottom = (parseFloat(circle.style.bottom) + 25) + 'px';
-    projectile.style.left = '50%';
-    projectile.style.transform = 'translateX(-50%)';
-    container.appendChild(projectile);
-
-    requestAnimationFrame(function animate() {
-        const currentBottom = parseFloat(projectile.style.bottom);
-        const sRect = sLetter.getBoundingClientRect();
-        const projRect = projectile.getBoundingClientRect();
-
-        // Check for collision between projectile and the "s" letter
-        if (projRect.top <= sRect.bottom && projRect.bottom >= sRect.top) {
-            sLetter.style.display = 'none'; // Hide the "s"
-        }
-
-        if (currentBottom < window.innerHeight) {
-            projectile.style.bottom = (currentBottom + 5) + 'px';
-            requestAnimationFrame(animate);
-        } else {
-            container.removeChild(projectile);
-        }
-    });
-
-    // Animate main circle back to original position
-    circle.style.transition = 'bottom 0.5s cubic-bezier(.25,.82,.25,1)';
-    circle.style.bottom = '70px';
-
-    // Remove transition once animation is complete
-    setTimeout(() => circle.style.transition = '', 500);
 });
