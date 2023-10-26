bb995f89f471d64e2d1529666468e634f06a7673
EasyJake <EasyJake@users.noreply.github.com>
2023-10-25 18:36:25 -0700

    Changed movement of  opponent so that it begins before the first shot is fired.

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('#mobile-container');
     const circle = document.createElement('div');
-    const redSquare = document.createElement('div');
-    const projectiles = []; // Store projectiles in an array
 
+    // Store red squares and projectiles in arrays
+    const redSquares = [];
+    const projectiles = [];
+
+    // Set up the player (blue circle)
     circle.style.width = '50px';
     circle.style.height = '50px';
     circle.style.backgroundColor = 'blue';
@@ -12,43 +15,59 @@ document.addEventListener('DOMContentLoaded', function() {
     circle.style.bottom = '70px';
     circle.style.left = '50%';
     circle.style.transform = 'translateX(-50%)';
-    circle.style.cursor = 'pointer'; // Cursor indicates it's draggable
-
-    redSquare.style.width = '50px';
-    redSquare.style.height = '50px';
-    redSquare.style.backgroundColor = 'red';
-    redSquare.style.position = 'absolute';
-    redSquare.style.top = '0';
-    redSquare.style.left = '-50px'; // Start off-screen to the left
-    redSquare.style.transform = 'translateX(-50%)';
+    circle.style.cursor = 'pointer';
 
     container.appendChild(circle);
-    container.appendChild(redSquare);
 
+    // Variables for dragging functionality
     let isDragging = false;
     let startDragY = 0;
     let startCircleBottom = 0;
-    const radius = 25; // Half of the circle's diameter
+    const radius = 25;
 
+    // Function to generate and animate red squares
+    function generateRedSquare() {
+        const redSquare = document.createElement('div');
+        redSquare.style.width = '50px';
+        redSquare.style.height = '50px';
+        redSquare.style.backgroundColor = 'red';
+        redSquare.style.position = 'absolute';
+        redSquare.style.top = '0';
+        redSquare.style.left = '-50px';
+        redSquare.style.transform = 'translateX(-50%)';
+
+        container.appendChild(redSquare);
+        redSquares.push(redSquare);
+
+        // Move the red square from left to right
+        function moveRedSquare() {
+            const currentLeft = parseFloat(redSquare.style.left);
+            if (currentLeft < window.innerWidth) {
+                redSquare.style.left = (currentLeft + 5) + 'px';
+                requestAnimationFrame(moveRedSquare);
+            } else {
+                redSquare.remove();
+                redSquares.splice(redSquares.indexOf(redSquare), 1);
+            }
+        }
+        moveRedSquare();
+    }
+
+    // Generate red squares periodically
+    setInterval(generateRedSquare, 2000);
+
+    // Event listeners for drag-and-release functionality
     circle.addEventListener('mousedown', function(event) {
         isDragging = true;
         startDragY = event.clientY;
         startCircleBottom = parseFloat(circle.style.bottom);
-        event.preventDefault(); // Prevent text selection, etc.
+        event.preventDefault();
     });
 
     document.addEventListener('mousemove', function(event) {
         if (!isDragging) return;
-
         let distanceMoved = startDragY - event.clientY;
-
-        // Limit the distance to the radius of the circle
-        if (distanceMoved > radius) {
-            distanceMoved = radius;
-        } else if (distanceMoved < -radius) {
-            distanceMoved = -radius;
-        }
-
+        distanceMoved = Math.max(Math.min(distanceMoved, radius), -radius);
         circle.style.bottom = (startCircleBottom + distanceMoved) + 'px';
     });
 
@@ -56,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!isDragging) return;
         isDragging = false;
 
-        // Create and animate the projectile
+        // Create and animate a projectile when the mouse is released
         const projectile = document.createElement('div');
         projectile.style.width = '25px';
         projectile.style.height = '25px';
@@ -66,59 +85,47 @@ document.addEventListener('DOMContentLoaded', function() {
         projectile.style.bottom = (parseFloat(circle.style.bottom) + 25) + 'px';
         projectile.style.left = '50%';
         projectile.style.transform = 'translateX(-50%)';
+
         container.appendChild(projectile);
+        projectiles.push(projectile);
 
-        projectiles.push(projectile); // Add projectile to the array
-
+        // Check for collisions between projectiles and red squares
         function checkCollision() {
-            const circleRect = circle.getBoundingClientRect();
-            const redSquareRect = redSquare.getBoundingClientRect();
-            const projectileRect = projectile.getBoundingClientRect();
+            projectiles.forEach((projectile, index) => {
+                const projectileRect = projectile.getBoundingClientRect();
 
-            if (
-                projectileRect.left < redSquareRect.right &&
-                projectileRect.right > redSquareRect.left &&
-                projectileRect.bottom > redSquareRect.top &&
-                projectileRect.top < redSquareRect.bottom
-            ) {
-                // Collision detected, remove the projectile and red square
-                container.removeChild(projectile);
-                container.removeChild(redSquare);
-                projectiles.pop(); // Remove projectile from the array
-            }
+                redSquares.forEach((redSquare, squareIndex) => {
+                    const redSquareRect = redSquare.getBoundingClientRect();
+                    if (projectileRect.left < redSquareRect.right &&
+                        projectileRect.right > redSquareRect.left &&
+                        projectileRect.top < redSquareRect.bottom &&
+                        projectileRect.bottom > redSquareRect.top) {
+                        redSquare.remove();
+                        redSquares.splice(squareIndex, 1);
+                        projectile.remove();
+                        projectiles.splice(index, 1);
+                    }
+                });
+            });
         }
 
-        requestAnimationFrame(function animateProjectile() {
+        // Animate the projectile
+        function animateProjectile() {
             const currentBottom = parseFloat(projectile.style.bottom);
             if (currentBottom < window.innerHeight) {
                 projectile.style.bottom = (currentBottom + 5) + 'px';
-                checkCollision(); // Check for collision on each frame
+                checkCollision();
                 requestAnimationFrame(animateProjectile);
             } else {
-                container.removeChild(projectile);
-                projectiles.pop(); // Remove projectile from the array
-            }
-        });
-
-        // Animate main circle back to original position
-        circle.style.transition = 'bottom 0.5s cubic-bezier(.25,.82,.25,1)';
-        circle.style.bottom = '70px';
-
-        // Remove transition once animation is complete
-        setTimeout(() => circle.style.transition = '', 500);
-
-        // Animate the red square from left to right
-        function animateRedSquare() {
-            const currentLeft = parseFloat(redSquare.style.left);
-            if (currentLeft < window.innerWidth) {
-                redSquare.style.left = (currentLeft + 5) + 'px';
-                requestAnimationFrame(animateRedSquare);
-            } else {
-                redSquare.style.left = '-50px'; // Reset to off-screen left
-                setTimeout(() => requestAnimationFrame(animateRedSquare), 1000); // Wait for 1 second before re-entering
+                projectile.remove();
+                projectiles.splice(projectiles.indexOf(projectile), 1);
             }
         }
+        requestAnimationFrame(animateProjectile);
 
-        requestAnimationFrame(animateRedSquare);
+        // Reset circle's position
+        circle.style.transition = 'bottom 0.5s cubic-bezier(.25,.82,.25,1)';
+        circle.style.bottom = '70px';
+        setTimeout(() => circle.style.transition = '', 500);
     });
 });
