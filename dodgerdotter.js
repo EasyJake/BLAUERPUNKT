9fe2229a36d4220d50a10f97f94861a5ae963c67
EasyJake <EasyJake@users.noreply.github.com>
2023-11-04 11:30:52 -0700

    Update dodgerdotter.js GAME OVER condition

--- dodgerdotter.js
+++ dodgerdotter.js
@@ -152,7 +152,7 @@ document.addEventListener('DOMContentLoaded', function() {
         winScreen.style.left = '0';
         winScreen.style.width = '100%';
         winScreen.style.height = '100%';
-        winScreen.style.backgroundColor = 'blue';
+        winScreen.style.backgroundColor = 'RED';
         winScreen.style.display = 'flex';
         winScreen.style.justifyContent = 'center';
         winScreen.style.alignItems = 'center';
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function() {
         winScreen.style.zIndex = '1000';
 
         const winText = document.createElement('div');
-        winText.textContent = 'YOU WIN';
+        winText.textContent = 'GAME OVER';
 
         const playAgainButton = document.createElement('button');
         playAgainButton.textContent = 'Play Again';
