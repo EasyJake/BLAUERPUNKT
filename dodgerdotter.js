d39f7504e0474cb38d676e650e27e41b90f5c182
EasyJake <EasyJake@users.noreply.github.com>
2023-11-04 11:38:35 -0700

    Added inzone to game play

--- dodgerdotter.js
+++ dodgerdotter.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('#mobile-container');
+    // ... rest of your code ...
+
+    // Define the height for the blue background at the top
+    const topBackgroundHeight = '150px'; // for example, adjust as needed
+
+    // Create the top background element
+    const topBackground = document.createElement('div');
+    topBackground.style.width = '100%';
+    topBackground.style.height = topBackgroundHeight;
+    topBackground.style.backgroundColor = 'blue';
+    topBackground.style.position = 'absolute';
+    topBackground.style.top = '0';
+    topBackground.style.left = '0';
+    topBackground.style.zIndex = '-1'; // To ensure it stays behind other elements
+
+    // Append the top background to the container
+    container.appendChild(topBackground);
+
+    // ... rest of your code ...
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('#mobile-container');
     const circle = document.createElement('div');
 
     // Store opponent shapes and projectiles in arrays
